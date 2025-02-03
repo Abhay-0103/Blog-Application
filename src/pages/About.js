72b9626be58e311d7958dd507593ea17736a098b
Abhay-0103 <ab0321054@gmail.com>
2025-02-03 21:37:32 +0530

    Node.js and express.js

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='mb-20'>
+    
+    <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+      About Me
+      </h1>
+    <p className="mx-auto leading-relaxed text-base mb-4">
+      hello hello hey hey
+      </p>
+    <p className="mx-auto leading-relaxed text-base mb-4">
+      meh hu Abhay Singhj nam tho suna hi hoga
+      </p>
+  </div>
+  );
+}
+
+export default About
